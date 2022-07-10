6b8fbb26d7215c2fc091093b822d6baaea4ae31f
saamon <suzuken2960@gmail.com>
2022-07-11 01:03:52 +0900

    初期ファイル作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>My Pixabay</h2>
+        <form>
+            <input type={"text"} placeholder={"画像を探す"}/>
+        </form>
     </div>
   );
 }
